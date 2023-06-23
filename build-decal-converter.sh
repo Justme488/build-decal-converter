4871e95ec9a591f1e1e280fa081d0daa341eefb2
Justme488 <justme488@gmail.com>
2023-06-23 13:21:25 -0400

    Update build-decal-converter.sh

--- build-decal-converter.sh
+++ build-decal-converter.sh
@@ -125,8 +125,7 @@ cd "${HOME}/Desktop"
 # If the .deb file exists, delete the build directory
 echo "Checking if decal-converter.deb package exists, and deleting temp folder & build-decal-converter.sh if it does"
 if [[ -e "${HOME}/Desktop/decal-converter.deb" ]]; then
-  rm -rf "${tmp_dir}"
-  rm "${HOME}/Desktop/build-decal-converter.sh"
+  rm -rf "${tmp_dir}" && rm -f "${HOME}/Desktop/build-decal-converter.sh"
 fi
 
 zenity --info --title="Install File Is On Your Desktop" --text="decal-converter.deb is on your desktop\n\n( ${HOME}/Desktop/decal-converter.deb )" --width="400" --height="100"
